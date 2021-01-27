7fb24a794eadf16bb6a0727b212039ce3c1777d1
ricardo aragon <ricardoaragon01@gmail.com>
2021-01-27 16:51:25 -0500

    Factorial functions

--- 03-loops/factorial.ex
+++ 03-loops/factorial.ex
@@ -1,13 +1,13 @@
 defmodule Factorial do
-  def classic(n)  when is_interger(n) and n >= 01 do
+  def classic(n)  when is_integer(n) and n >= 0 do
     cond do
       n < 2 -> 1
-      true -> fac1(n, 0, 1)
+      true -> facp1(n, 0, 1)
     end
   end
 
   def fac1(n) when is_integer(n) and n >= 0 do
-    fac1(n, 0, 1)
+    facp1(n, 0, 1)
   end
 
   # acc = 1;
@@ -26,4 +26,40 @@ defmodule Factorial do
       true -> facp1(n, i + 1, acc * (i + 1))
     end
   end
+
+  #fac2(n) = Acc(n, k) * fac2(n - k)
+  # Acc(n, k) = n * (n-1) * ... * (n - k + 1)
+  # Acc(n, k) = acc(n, k.1) * (n - k + 1)
+  def fac2(n) when is_integer(n) and n >= 0 do
+    cond do
+      n < 2 -> 1
+      true -> facp2(n - 1, n)
+    end
+  end
+
+  # defp facp2(n, acc) when is_integer(n) and n == 0 do
+  #    acc
+  # end
+  defp facp2(0, acc) when is_integer(acc) do
+    acc
+  end
+
+  defp facp2(n, acc) when is_integer(n) do
+    facp2(n - 1, acc * n)
+  end
+
 end
+
+
+IO.puts (Factorial.classic(6))
+IO.puts (" fac1(0) = #{Factorial.fac1(0)}")
+IO.puts (" fac1(1) = #{Factorial.fac1(1)}")
+IO.puts (" fac1(2) = #{Factorial.fac1(2)}")
+IO.puts (" fac1(3) = #{Factorial.fac1(3)}")
+IO.puts (" fac1(4) = #{Factorial.fac1(4)}")
+IO.puts (" fac1(5) = #{Factorial.fac1(5)}")
+IO.puts("---------------------")
+IO.puts (" fac2(0) = #{Factorial.fac2(0)}")
+IO.puts (" fac2(1) = #{Factorial.fac2(1)}")
+IO.puts (" fac2(2) = #{Factorial.fac2(2)}")
+IO.puts (" fac2(3) = #{Factorial.fac2(3)}")
