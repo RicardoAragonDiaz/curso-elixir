4a33f95076790f56ed4b213eafd79f2f87943858
ricardo aragon <ricardoaragon01@gmail.com>
2021-01-27 16:36:42 -0500

    Taller Collatz

--- 03-loops/Collatz.ex
+++ 03-loops/Collatz.ex
@@ -1,10 +1,10 @@
 defmodule Collatz do
   def coll(n)  when is_integer(n)  and n > 0 do
     cond do
-      n == 1 -> 0
-      rem(n, 2) == 1 -> IO.puts(n)
+      n == 1 -> IO.puts("#{n}")
+      rem(n, 2) == 1 -> IO.puts("#{n}")
         1 + coll(n * 3 + 1)
-      rem(n, 2) == 0 -> IO.puts(n)
+      rem(n, 2) == 0 -> IO.puts("#{n}")
         1 + coll(div(n, 2))
     end
   end
